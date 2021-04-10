a7ef06f56cf60b95d155a9b5e80a798756763127
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2021-04-10 12:12:47 +0700

    arrow function

---
+++ arrow-function.js
@@ -0,0 +1,45 @@
+// Sự khác nhau giữa Function thông thường và Arrow Function
+// Function thông thường
+let regularObj = function() {
+  this.name = 'An Vu';
+  return {
+    name: 'John',
+    getName: function() {
+      // this sẽ bị ghi đè vì this lúc này thuộc về Obj được trả về
+      return this.name;
+    }
+  };
+}
+console.log('regularObj: ' + regularObj().getName());
+
+// Arrow Function
+let arrowObj = function() {
+  this.name = 'An Vu';
+  return {
+    name: 'John',
+    getName: () => {
+      // this này không bị ghi đè bởi Obj mà vẫn giữ được this của hàm cha
+      return this.name;
+    }
+  };
+}
+console.log('arrowObj: ' + arrowObj().getName());
+
+
+/* 
+Bai tap 1:
+Cho 1 array tu 0 => 10. Viet 1 arrow function de multiple cac element trong array do.
+
+Bai tap 2:
+- generate 1 new array like object:
+- const array = [ 3, 4, 5 ….]: co 10 phan tu
+- field name se~ dc generate random theo alphabet.
+- field price se~ dc la cac element trong array * 10
+
+ex:
+[
+   { name: dasesa, price: i * 10 },
+   …..
+]
+in ra cac object nao` co price > 50 & < 100.
+*/
