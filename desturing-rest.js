94781656ead8203f55be75af93f8a6c0884a8006
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2021-04-11 21:28:31 +0700

    destru

--- desturing-rest.js
+++ desturing-rest.js
@@ -1,15 +1,16 @@
 const objA = {
-  abc: 1,
+  a: 1,
   b: 2,
-  c: 3
+  t: 3 
 }
 
 const objB = {
   t: 1,
-  y: 2
+  y: 2,
+  ...objA
 }
 
 
-const combineObj = { ...objA, ...objB};
+// const combineObj = { ...objA, ...objB};
 
-console.log(combineObj)
+console.log(objB)
