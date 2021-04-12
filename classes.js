afa1e06e1b7a3831ec4d88a178d26e93ed9cc24c
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2021-04-12 22:10:30 +0700

    learn

---
+++ classes.js
@@ -0,0 +1,17 @@
+class Person {
+  constructor(url, car = 'honda') {
+    this.url = url;
+    this.car = car
+  }
+  setItem() {
+    this.car = 'bmw'
+    //
+  }
+  getItem() {
+    return this.car
+  }
+}
+
+const person = new Person('localhost: 4000');
+// person.setItem();
+console.log('person: ', person.getItem())
