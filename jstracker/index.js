a45c8d65ceb07d64c62fc855bb0691ec2d72ee14
Tuyen Van <emux189@gmail.com>
2021-05-11 20:08:10 -0600

    bootstrap

--- jstracker/index.js
+++ jstracker/index.js
@@ -21,8 +21,8 @@ const user = JSON.parse(getUser);
 
 loading.style.display = 'block';
 fetch(`https://tony-json-server.herokuapp.com/api/todos`, {
-    method: 'GET',
-  })
+  method: 'GET',
+})
   .then(res => res.json())
   .then(res => {
     loading.style.display = 'none';
@@ -34,7 +34,7 @@ fetch(`https://tony-json-server.herokuapp.com/api/todos`, {
 
 // add new todo
 const todoForm = document.getElementById('todoForm');
-todoForm.addEventListener('submit', function(e) {
+todoForm.addEventListener('submit', function (e) {
   e.preventDefault();
   const description = document.getElementById('description').value;
   const severity = document.getElementById('severity').value;
@@ -58,86 +58,119 @@ todoForm.addEventListener('submit', function(e) {
   fetch("https://tony-json-server.herokuapp.com/api/todos", {
     method: "POST",
     headers: {
-        'Content-Type': 'application/json'
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify(newTodo)
   })
-  .then(_ => {
-    //$('.modal').show();
-    loading.style.display = 'none';
-    todoForm.reset();
-    listTodo.push(newTodo);
-    fetchTodos(listTodo);
-    changeColor();
-  })
+    .then(_ => {
+      //$('.modal').show();
+      loading.style.display = 'none';
+      todoForm.reset();
+      listTodo.push(newTodo);
+      fetchTodos(listTodo);
+      changeColor();
+    })
 })
+
 //change color of the status
 function changeColor() {
   const option = document.getElementsByTagName('option').value;
-  option[0].backgroundColor='black'
-  option[1].backgroundColor='green'
-  option[2].backgroundColor='red'
+  option[0].backgroundColor = 'black'
+  option[1].backgroundColor = 'green'
+  option[2].backgroundColor = 'red'
 }
 
 
 // update todo open->closed
 function setStatus(todoId) {
-  const status = document.getElementsByClassName("status");
-  for (const index in listTodo) { 
+  // document.getElementById("close-button").disabled = true;
+  for (const index in listTodo) {
     if (listTodo[index].id === todoId) {
-      if(listTodo[index].status === 'open') {
+      if (listTodo[index].status === 'open') {
+        loading.style.display = 'block';
         fetch(`https://tony-json-server.herokuapp.com/api/todos/${todoId}`, {
-            method: "PATCH",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                status: "close"
-            })
-        })
-        listTodo[index].status = "close";
-        status[index].textContent = "close";
-      } else {
-        fetch(`https://tony-json-server.herokuapp.com/api/todos/${todoId}`, {
-          method: "PATCH",
+          method: 'PATCH',
           headers: {
-              'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-              status: "open"
+            status: 'closed'
           })
         })
-        listTodo[index].status = "open";
-        status[index].textContent = "open";
+          .then(_ => {
+            listTodo[index].status = 'closed';
+            loading.style.display = 'none';
+            fetchTodos(listTodo);
+          })
       }
-      fetchTodos(listTodo);
     }
+  }
+  $todoId.setStatus = function (btn) {
+    btn.target.disabled = true;
   }
 }
 
 //filter status: open/closed
-const filterOptions = document.getElementById("filter");
+const filterOptions = document.querySelector(".filter-todos");
 filterOptions.addEventListener("change", filterTodos);
+
 function filterTodos(e) {
-  const option = e.target.value;
-  // clone list todos
-  switch (e.target.value) {
-    case "open": {
-      const newTodos = listTodo.filter(todo => todo.status === option)
-      fetchTodos(newTodos);
-      break;
-    }
-    case "close": {
-      const newTodos = listTodo.filter(todo => todo.status === option)
-      fetchTodos(newTodos);
-      break;
-    }
-    default: {
-      fetchTodos(listTodo);
-      break;
+  for (const index in listTodo) {
+    switch (e.target.value) {
+      case "open":
+        if ((listTodo[index].status) === 'open') {
+          loading.style.display = 'block';
+          fetch(`https://tony-json-server.herokuapp.com/api/todos${todoId}`), {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              status: 'open'
+            })
+          }
+            .then(_ => {
+              // boxTodos.style.display = "flex";
+              // loading.style.display = 'none';
+              // fetchTodos(listTodo);
+              listTodo.filter((listTodo) => listTodo[index].status === 'open')
+              fetchTodos(listTodo);
+            })
+        }
+        else {
+          boxTodos.style.display = "none"
+        }
+        break;
+
+      case "closed":
+        if ((listTodo[index].status) === 'closed') {
+          loading.style.display = 'block';
+          fetch(`https://tony-json-server.herokuapp.com/api/todos${todoId}`), {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              status: 'closed'
+            })
+          }
+            .then(_ => {
+              // boxTodos.style.display = "flex";
+              // loading.style.display = 'none';
+              // fetchTodos(listTodo);
+              listTodo.filter((listTodo) => listTodo[index].status === 'closed')
+              fetchTodos(listTodo);
+            })
+        }
+        else {
+          boxTodos.style.display = "none"
+        }
+        break;
     }
   }
-} 
+}
+
+
 
 // delete todo
 function deleteTodo(todoId) {
@@ -145,15 +178,15 @@ function deleteTodo(todoId) {
   fetch(`https://tony-json-server.herokuapp.com/api/todos/${todoId}`, {
     method: 'DELETE'
   })
-  .then(_ => {
-    loading.style.display = 'none';
-    for (const index in listTodo) {
-      if(listTodo[index].id === todoId) {
-        listTodo.splice(index, 1);
-        fetchTodos(listTodo);
+    .then(_ => {
+      loading.style.display = 'none';
+      for (const index in listTodo) {
+        if (listTodo[index].id === todoId) {
+          listTodo.splice(index, 1);
+          fetchTodos(listTodo);
+        }
       }
-    }
-  })
+    })
 }
 
 // delete all 
@@ -167,10 +200,10 @@ document.getElementById('deleteAll').addEventListener('click', event => {
 function fetchTodos(list) {
   const boxTodos = document.getElementById('boxTodos');
   boxTodos.innerHTML = '';
-  
+
   for (const index in list) {
     boxTodos.innerHTML += `<div class="box"><p class="id-title">Issue ID: <span class="id">bc71f535-a73c-5edf-b62d-351d3125fd1f</span></p>
-      <p class="btn btn-info filterDiv status">${list[index].status}</p>
+      <button class="btn btn-info filterDiv">${list[index].status}</button>
       <div class="issue-name">${list[index].description}</div>
       <div class="severity">
         <img src="https://img.icons8.com/pastel-glyph/64/000000/clock--v1.png"/>
