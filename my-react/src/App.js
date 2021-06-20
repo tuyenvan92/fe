37ec0cb98ec7158e1a249f7899f1da86911c252a
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2021-06-20 11:16:57 +0700

    my react

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,13 +1,70 @@
+import { Fragment } from 'react';
 
-function App() {
-  const element = <h1>hello word</h1>;
 
+const dataUsers = {
+  firstName: 'nguyen',
+  lastName: 'tony'
+}
+
+function CompomentA(props) {
   return (
     <div>
-      this is app {element}
-      2 + 2
+      this is component A
     </div>
   )
+}
+
+
+function App() {
+  // way 1 element:
+  const element = <h1>hello word</h1>;
+  // way 2 element:
+  const element_2 = (
+    <h1>hello word</h1>
+  )
+
+  const num = 2 + 2;
+  const formatUsers = (users) => {
+    return `${users.lastName} ${users.firstName}`
+  }
+
+  const renderGetting = user => {
+    if(user) {
+      return (
+        <Fragment>
+          <div>Hello {user}</div>
+          <div>Hello 2 {user}</div>
+          <div>Hello 3 {user}</div>
+        </Fragment>
+      )
+    }
+
+    return (
+      <div>hello strange</div>
+    )
+  }
+ 
+  return (
+    <div>
+      this is app {element} <br/> {element_2}
+      {num}
+      <br />
+      {formatUsers(dataUsers)}
+
+      
+      {renderGetting('tuyen')}
+
+      <img src="abc" className="image_default" alt="docs" />
+
+      <br />
+
+      <h3>Render JSX with React function component</h3>
+      <CompomentA />
+      
+     
+    </div>
+  )
+  
 }
 
 export default App;
