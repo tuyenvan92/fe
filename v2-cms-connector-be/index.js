2c812b46cc3b6cb6b22d92214072043934bfc9f4
Tony Nguyen <tony.nguyen@usonialabs.com>
2022-02-26 12:45:30 +0700

    done

--- v2-cms-connector-be/index.js
+++ v2-cms-connector-be/index.js
@@ -4,37 +4,29 @@ const cors = require('cors')
 const app = express();
 const mongoose = require('mongoose')
 
+const mongo = require('./mongo');
+
 const userRoute = require('./routes/userRoute')
 dotenv.config()
 app.use(cors());
 
-// mongoose.connect( 
-//     'mongodb+srv://nhattruongniit:truong123@cluster0.xga7w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-//     { 
-//         useNewUrlParser: true, 
-//         useUnifiedTopology: true,
-//         useFindAndModify: false
-//       },
-//     () => {
-//         console.log('connect to DB')
-//     }
-// )
 mongoose.connect(`mongodb+srv://tuyenvan:tuyen123@cluster0.gtqk2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
         useNewUrlParser: true, 
         useUnifiedTopology: true
 }).then(() => {
     console.log('MongoDB connected!!');
-}).catch(err => {
-    console.log('Failed to connect to MongoDB', err);
-});
-
+    
 const PORT = process.env.PORT || 3005
 
-app.use(express.json({ extend:true}));
-app.get('/', (_,res) => res.send('API running'));
+mongo().then(() => {
 
-app.use('/api/user', userRoute)
+    app.use(express.json({ extend:true}));
+    app.get('/', (_,res) => res.send('API running'));
+    
+    app.use('/api/user', userRoute)
+    
+    app.listen(PORT, () => {
+        console.log(`Server up and running localhost: ${PORT}`)
+    });
+})
 
-app.listen(PORT, () => {
-    console.log(`Server up and running localhost: ${PORT}`)
-});
